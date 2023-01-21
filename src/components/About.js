491e130fcfe5d646bb26319eceb22d4331b72a4a
oyeniran samuel sanmi <sossesam@gmail.com>
2023-01-21 23:27:22 +0100

    all

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,7 @@ const About = () => {
           quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
           unde dolor?
         </p>
-        <a href="#" className="btn">read more</a>
+        <a href="#About"className="btn">read more</a>
       </article>
     </div>
   </section>
