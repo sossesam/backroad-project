e66bc70f4b08eba5da5719dcc9908fd0c4a037f3
oyeniran samuel sanmi <sossesam@gmail.com>
2023-01-21 13:47:06 +0100

    all

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+import About  from './components/About';
+import Services from './components/Services';
+import Tours from './components/Tours';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <h1>Our backroads website</h1>
+    <>
+    <NavBar/>
+    <Hero/>
+    <About/>
+    <Services />
+    <Tours />
+    <Footer/>
+    <script src="./js/app.js"></script></>
+
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,8 @@
+
+export const pageLinks = [
+    {id:1, href:'#home', text:'Home'},
+    {id:2, href:'#about', text:'About'},
+    {id:3, href:'#services', text:'Services'},
+    {id:4, href:'#tours', text:'Tours'},
+    
+]
